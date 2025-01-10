21a632b2e0dfb9ca37be3d35eaff11952906c19a
Endermanik13 <119730649+Endermanik13@users.noreply.github.com>
2025-01-10 23:10:50 +0300

    10.01.2024

---
+++ code.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+void printMaze(int maze[][7], int rows, int cols) { // Вывод карты
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << maze[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    int maze[5][7] = {
+        {1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 2, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1}
+    };
+    int playerX = 2;
+    int playerY = 3;
+
+    while (true) {
+        cout << "Выберите функцию" << endl;
+        cout << "[1] Посмотреть карту" << endl;
+        cout << "[2] Управление" << endl;
+        cout << "[0] Выход" << endl;
+
+        int command;
+        cout << "Введение команды: ";
+        cin >> command;
+
+        if (command == 0) {
+            system("cls");
+            cout << "Выход из программы. До свидания!" << endl;
+            break;
+        }
+
+        if (command == 1) {
+            system("cls");
+            printMaze(maze, 5, 7);
+        }
+        else if (command == 2) {
+            while (true) {
+                system("cls");
+                printMaze(maze, 5, 7);
+
+                cout << "Выберите направление" << endl;
+                cout << "[1] Вверх" << endl;
+                cout << "[2] Влево" << endl;
+                cout << "[3] Вниз" << endl;
+                cout << "[4] Вправо" << endl;
+                cout << "[0] Вернуться в меню" << endl;
+
+                int direction;
+                cout << "Введите направление: ";
+                cin >> direction;
+
+                if (direction == 0) {
+                    system("cls");
+                    break;
+                }
+
+                int newX = playerX;
+                int newY = playerY;
+                if (direction == 1) newX--;
+                else if (direction == 2) newY--;
+                else if (direction == 3) newX++;
+                else if (direction == 4) newY++;
+
+                if (maze[newX][newY] == 0) {
+                    maze[playerX][playerY] = 0;
+                    maze[newX][newY] = 2;
+                    playerX = newX;
+                    playerY = newY;
+
+                    system("cls");
+                    printMaze(maze, 5, 7);
+                }
+                else {
+                    cout << "Невозможно двигаться в выбранном направлении!" << endl;
+                }
+            }
+        }
+        else {
+            cout << "Неверная команда, попробуйте снова." << endl;
+        }
+    }
+
+    return 0;
+}
